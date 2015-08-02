daa955ff5d4211cfe1c0b4c7d24f3a6c36e40788
YeMyat Thein <yemyatthein@gmail.com>
2015-08-02 19:28:12 +0100

    docstring added

--- timely_view.py
+++ timely_view.py
@@ -26,17 +26,31 @@ class TimelyView(object):
             self._compare_start_end(start, end)
             
     def _compare_start_end(self, start, end):
+        ''' Find global min/max of provided input data '''
+
         if not self.min_date or start < self.min_date:
             self.min_date = start
         if not self.max_date or end > self.max_date:
             self.max_date = end
             
     def _distribute_data(self):
+        '''
+        Given projects and min/max (start/end) of all projects, distribute data
+        value according to the time axis (Panda terminology). After this method,
+        the panda DataFrame will have time as indexes (vertical rows) and
+        projects' names as columns (horizontal columns).
+
+        The meaning is each row is one Date, and in that row each project's
+        value share for that particular date.
+        '''
+
         ls = self.datalines
         num_rows = (self.max_date - self.min_date).days + 1
         dindex = pd.date_range(self.min_date, self.max_date, freq='D')
         cols = [x[0] for x in ls]
-        self.pdtable = pd.DataFrame([[0. for _ in xrange(len(ls))] for x in xrange(num_rows)], index=dindex, columns=cols)
+        self.pdtable = pd.DataFrame(
+                    [[0. for _ in xrange(len(ls))] for x in xrange(num_rows)],
+                    index=dindex, columns=cols)
 
         for i, x in enumerate(ls):
             num_slots = (x[2] - x[1]).days + 1
@@ -45,6 +59,13 @@ class TimelyView(object):
                 self.pdtable.ix[x[1] + timedelta(j)][cols[i]] = value_share
         
     def _calculate_aggregations(self):
+        '''
+        This method transforms panda DataFrame resulting from _distribute_data
+        to a new DataFrame which has same number of rows but only one column
+        containing RowData object. RowData object holds calculated aggregation
+        types (SUM, AVG, MIN, MAX, etc) of each Date.
+        '''
+
         df = self.pdtable
         
         def get_sum(ls):
@@ -71,7 +92,8 @@ class TimelyView(object):
         def get_count(ls):
             return len([x for x in ls if x > 0])
         
-        self.pdresult = pd.DataFrame([None for _ in xrange(len(df))], index=df.index)
+        self.pdresult = pd.DataFrame([None for _ in xrange(len(df))],
+                                    index=df.index)
         for i in xrange(len(df)):
             rowlist = df.ix[i].tolist()
 
@@ -81,10 +103,16 @@ class TimelyView(object):
             mnindex, minimum = get_min(rowlist)
             count = get_count(rowlist)
 
-            self.pdresult.ix[i] = RowData(summation, average, maximum, minimum, count)
+            self.pdresult.ix[i] = RowData(summation, average,
+                                        maximum, minimum, count)
         return self.pdresult
             
     def set_input_from_file(self, filename):
+        '''
+        Expect CSV file in which each line has this format.
+        [IDENTIFYING_NAME, START_DATE, END_DATE, VALUE]
+        '''
+
         with open(filename, 'rb') as f:
             for i, x in enumerate(f):
                 if i > 10000:
@@ -102,6 +130,12 @@ class TimelyView(object):
         self._calculate_aggregations()
         
     def aggregation_between(self, start, end, agg_type):
+        '''
+        Assuming that, _calculate_aggregations method is already called,
+        this method returns the rows and aggregation type of those rows
+        requested by callers.
+        '''
+
         to_get = (end - start).days + 1
         sindex = (start - self.min_date).days
         eindex = sindex + to_get
@@ -125,4 +159,5 @@ class RowData(object):
         self.mxresp = mxresp
         self.mnresp = mnresp
     def __repr__(self):
-        return str((self.summation, self.average, self.maximum, self.minimum, self.count))
+        return str((self.summation, self.average, self.maximum,
+                    self.minimum, self.count))
